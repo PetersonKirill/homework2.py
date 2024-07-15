3b04f11fa3aa8fdc17ff5616cd2ca0942086e6a3
Peterson Kirill <145568807+PetersonKirill@users.noreply.github.com>
2024-07-15 23:39:30 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,9 @@
+course = "Python"
+all_tasks = 12
+hours_spent = 1.5
+average_execution_time = 0.125
+
+print ("Курс:", course + ",", 
+       "всего задач:" + str(all_tasks) + ",", 
+       "затрачено часов:", str(hours_spent) + ",",
+       "среднее время выполнения", average_execution_time, "часа")
